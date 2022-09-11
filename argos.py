ac40fd15fed26bfc9e999acaf10f3797e339a9f7
oscar <oscarherediallorens@gmail.com>
2022-09-11 12:47:13 +0000

    Change pydoc format to google standard

--- argos.py
+++ argos.py
@@ -3,26 +3,28 @@
 import csv
 import json
 
-# Input:
-#   CSVFile: path to a csv file
-#   JSONFile: pat to a JSON file our function will create
-# Output:
-#   None. JSONFile is created
+
 def createJSON(CSVFile, JSONFile):
-   
+    """createJSON.
+
+    Args:
+        CSVFile: path to a CSV file
+        JSONFile: path to a JSON file our function will create as output
+    """
+
     # output dict
     outputdict = {}
 
     # Open CSV for read and store it to our JSON
     with open(CSVFile, encoding='utf=8') as csvFP:
-       csvReader = csv.DictReader(csvFP)
+        csvReader = csv.DictReader(csvFP)
 
-       for rows in csvReader:
+        for rows in csvReader:
             # First column must be "CK"
             key = rows['CK']
             outputdict[key] = rows
 
-    # Once we have all our data, generate our output file JSONFile 
+    # Once we have all our data, generate our output file JSONFile
     with open(JSONFile, 'w', encoding='utf=8') as jsonFP:
         jsonFP.write(json.dumps(outputdict, indent=4))
 
